31553e1f7586c33e6add959792cf19d8a50b7cb4
noy72 <kuma.tola3@gmail.com>
2020-06-12 14:46:14 +0900

    Add html

--- bookmaker/app/main.js
+++ bookmaker/app/main.js
@@ -1,5 +1,9 @@
-const {app} = require('electron');
+const {app, BrowserWindow} = require('electron');
+
+let mainWindow = null;
 
 app.on('ready', () => {
     console.log("hello world");
+    mainWindow = new BrowserWindow();
+    mainWindow.webContents.loadFile('./app/index.html');
 });
